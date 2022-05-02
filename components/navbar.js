aa54f5d025dd1b23b06b12770b8c9cb19b4321a5
Majid Sayed <sydmajid04@gmail.com>
2022-05-02 11:09:34 +0530

    final progress index page

--- components/navbar.js
+++ components/navbar.js
@@ -4,3 +4,35 @@ function navbar(){
 }
 
 export {navbar}
+
+function append(x){
+    x.forEach(el => {
+        let results = document.getElementById('results')
+
+        let mainBox = document.createElement('div')
+        mainBox.style.display = 'flex';
+        mainBox.setAttribute('class', 'news')
+
+        let imgBox = document.createElement('div')
+        
+        let textBox = document.createElement('div')
+
+        let img = document.createElement('img')
+        img.src = el.urlToImage
+        img.style.height = '300px'
+        img.style.width = '300px'
+
+        let title = document.createElement('h3')
+        title.textContent = el.title
+
+        let script = document.createElement('p')
+        script.textContent = el.description
+
+        imgBox.append(img)
+        textBox.append(title, script)
+        mainBox.append(imgBox, textBox)
+        results.append(mainBox)
+    });
+}
+
+export {append}
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 // Ude Import export (MANDATORY)
 // Onclicking store the news in local storage with key "news" so that you can access that on news.html page
-import { navbar } from "../components/navbar.js";
+import { navbar, append} from "../components/navbar.js";
 document.getElementById('navbar').innerHTML = navbar();
 
 document.getElementById('in').addEventListener('click', newsIn)
@@ -15,34 +15,6 @@ function newsIn(){
     .then((data) => append(data.articles))
     .catch(err => console.log('Error: ', err))
 
-    function append(x){
-        x.forEach(el => {
-            let results = document.getElementById('results')
-
-            let mainBox = document.createElement('div')
-            mainBox.style.display = 'flex';
-
-            let imgBox = document.createElement('div')
-            
-            let textBox = document.createElement('div')
-
-            let img = document.createElement('img')
-            img.src = el.urlToImage
-            img.style.height = '300px'
-            img.style.width = '300px'
-
-            let title = document.createElement('h3')
-            title.textContent = el.title
-
-            let script = document.createElement('p')
-            script.textContent = el.description
-
-            imgBox.append(img)
-            textBox.append(title, script)
-            mainBox.append(imgBox, textBox)
-            results.append(mainBox)
-        });
-    }
 }
 
 document.getElementById('ch').addEventListener('click', newsCh)
@@ -55,34 +27,6 @@ function newsCh(){
     .then((data) => append(data.articles))
     .catch(err => console.log('Error: ', err))
 
-    function append(x){
-        x.forEach(el => {
-            let results = document.getElementById('results')
-
-            let mainBox = document.createElement('div')
-            mainBox.style.display = 'flex';
-
-            let imgBox = document.createElement('div')
-            
-            let textBox = document.createElement('div')
-
-            let img = document.createElement('img')
-            img.src = el.urlToImage
-            img.style.height = '300px'
-            img.style.width = '300px'
-
-            let title = document.createElement('h3')
-            title.textContent = el.title
-
-            let script = document.createElement('p')
-            script.textContent = el.description
-
-            imgBox.append(img)
-            textBox.append(title, script)
-            mainBox.append(imgBox, textBox)
-            results.append(mainBox)
-        });
-    }
 }
 
 document.getElementById('us').addEventListener('click', newsUs)
@@ -95,34 +39,6 @@ function newsUs(){
     .then((data) => append(data.articles))
     .catch(err => console.log('Error: ', err))
 
-    function append(x){
-        x.forEach(el => {
-            let results = document.getElementById('results')
-
-            let mainBox = document.createElement('div')
-            mainBox.style.display = 'flex';
-
-            let imgBox = document.createElement('div')
-            
-            let textBox = document.createElement('div')
-
-            let img = document.createElement('img')
-            img.src = el.urlToImage
-            img.style.height = '300px'
-            img.style.width = '300px'
-
-            let title = document.createElement('h3')
-            title.textContent = el.title
-
-            let script = document.createElement('p')
-            script.textContent = el.description
-
-            imgBox.append(img)
-            textBox.append(title, script)
-            mainBox.append(imgBox, textBox)
-            results.append(mainBox)
-        });
-    }
 }
 
 document.getElementById('uk').addEventListener('click', newsUk)
@@ -135,34 +51,6 @@ function newsUk(){
     .then((data) => append(data.articles))
     .catch(err => console.log('Error: ', err))
 
-    function append(x){
-        x.forEach(el => {
-            let results = document.getElementById('results')
-
-            let mainBox = document.createElement('div')
-            mainBox.style.display = 'flex';
-
-            let imgBox = document.createElement('div')
-            
-            let textBox = document.createElement('div')
-
-            let img = document.createElement('img')
-            img.src = el.urlToImage
-            img.style.height = '300px'
-            img.style.width = '300px'
-
-            let title = document.createElement('h3')
-            title.textContent = el.title
-
-            let script = document.createElement('p')
-            script.textContent = el.description
-
-            imgBox.append(img)
-            textBox.append(title, script)
-            mainBox.append(imgBox, textBox)
-            results.append(mainBox)
-        });
-    }
 }
 
 document.getElementById('nz').addEventListener('click', newsnz)
@@ -175,33 +63,5 @@ function newsnz(){
     .then((data) => append(data.articles))
     .catch(err => console.log('Error: ', err))
 
-    function append(x){
-        x.forEach(el => {
-            let results = document.getElementById('results')
-
-            let mainBox = document.createElement('div')
-            mainBox.style.display = 'flex';
-
-            let imgBox = document.createElement('div')
-            
-            let textBox = document.createElement('div')
-
-            let img = document.createElement('img')
-            img.src = el.urlToImage
-            img.style.height = '300px'
-            img.style.width = '300px'
-
-            let title = document.createElement('h3')
-            title.textContent = el.title
-
-            let script = document.createElement('p')
-            script.textContent = el.description
-
-            imgBox.append(img)
-            textBox.append(title, script)
-            mainBox.append(imgBox, textBox)
-            results.append(mainBox)
-        });
-    }
 }
 
