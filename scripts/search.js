0ac5b174a5fefc48f7adcd3a5815c5b0a11a54fb
Majid Sayed <sydmajid04@gmail.com>
2022-05-02 11:34:00 +0530

    progress

--- scripts/search.js
+++ scripts/search.js
@@ -1,2 +1,10 @@
 // Ude Import export (MANDATORY)
 // Onclicking store the news in local storage with key "news" so that you can access that on news.html page
+
+import { navbar } from "../components/navbar.js";
+document.getElementById('navbar').innerHTML = navbar();
+
+let a = document.getElementById('headingName')
+a.addEventListener('click', function(){
+    window.location.href = 'index.html'
+})
