528ad13b3112002ff768df452ff226f7ba353e95
Majid Sayed <sydmajid04@gmail.com>
2022-05-02 11:31:57 +0530

    progress

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,5 @@
 function navbar(){
-    return `<h3>News App</h3>
+    return `<h1 id='headingName'>News App</h1>
     <input type="text" id="search_input" placeholder="Search">`
 }
 
--- scripts/news.js
+++ scripts/news.js
@@ -1,1 +1,8 @@
 // Ude Import export (MANDATORY)
+import { navbar } from "../components/navbar.js";
+document.getElementById('navbar').innerHTML = navbar();
+
+let a = document.getElementById('headingName')
+a.addEventListener('click', function(){
+    window.location.href = 'index.html'
+})
